330ac5ee22e8c84ba08d7506d5eb7449385b401f
Nguyen Hau <nguyenhau230699@gmail.com>
2022-12-21 15:37:19 +0700

    upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = "🥰🥰🥰";
+const giftUrl = "https://www.facebook.com/huyen.nguyenkhanh.9003";
+const eventName = "Merry Christmas";
+const titleCard = "Message from Dinh Hau :>";
+const contentCard =
+  "Hello Khánh Huyền 😊.Chúc em pé có một mùa giáng sinh vui vẻ ấm áp. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. 💘chúc bé sớm có được anh :>💘";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+const giftImage = "./img/babi.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
